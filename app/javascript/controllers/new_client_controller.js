876e6277e98e4b7f2c1cc6af3f0c0b44caa3bd24
RubyOn <info@rubyon.co.kr>
2025-07-18 23:39:41 +0900

    wg 포트 자동 업데이트

--- app/javascript/controllers/new_client_controller.js
+++ app/javascript/controllers/new_client_controller.js
@@ -34,7 +34,13 @@ export default class extends Controller {
       if (this.hasInterfaceListenPortTarget) {
         this.interfaceListenPortTarget.textContent = selectedInterface.listen_port || '설정되지 않음'
       }
-      
+
+      const endpointInput = this.element.querySelector("input[name='client[endpoint]'], input[name='endpoint']")
+      if (endpointInput && selectedInterface.listen_port) {
+        const baseHost = endpointInput.value.replace(/:\d+$/, '') // 기존 포트 제거
+        endpointInput.value = `${baseHost}:${selectedInterface.listen_port}`
+      }
+
       this.interfaceInfoTarget.classList.remove('hidden')
     } else if (this.hasInterfaceInfoTarget) {
       this.interfaceInfoTarget.classList.add('hidden')
